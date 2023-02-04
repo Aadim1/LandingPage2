f44ecfb1f60022f03d6d2084838cc347228e04f8
Aadim_Chaulagain <iamaadim5@gmail.com>
2023-02-04 11:33:33 -0800

    Almost done with the UI, not responsive yet

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
-const dropdownMenu = document.querySelector(".dropdownMenu");
-const values = document.querySelector(".values");
-console.log(dropdownMenu);
-
-dropdownMenu.onclick = function(){
-
-    if (values.style.visibility === "visible"){
-        values.style.visibility = "hidden";
-    }else{
-        values.style.visibility = "visible";
-        values.style.opacity = 1;
-    }
-    
-}
+console.log(false === false)
